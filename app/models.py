71b33c53b59dae217bb2b380e0e7ac0f8a5704da
Moshood Sanusi <sanusimoshoodabiola@gmail.com>
2026-03-10 18:00:48 +0100

    update

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ import math
 from datetime import datetime
 from flask_login import UserMixin
 from slugify import slugify
+from flask_ckeditor import CKEditorField
 from .extensions import db
 
 # -------------------------
@@ -111,7 +112,6 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    # Relationship for likes
     liked_by = db.relationship(
         "User",
         secondary="post_likes",
@@ -121,18 +121,44 @@ class Post(db.Model):
     # -------------------------
     # Methods
     # -------------------------
+
+    def generate_unique_slug(self):
+        """
+        Generates a unique slug for the post based on the title.
+        If the slug already exists, appends a number to make it unique.
+        """
+        base_slug = slugify(self.title)
+        slug = base_slug
+        counter = 1
+
+        while Post.query.filter_by(slug=slug).first():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+
+        self.slug = slug
+
     def calculate_reading_time(self):
+        """
+        Calculates reading time based on average 200 words per minute.
+        """
         if self.content:
             self.reading_time = max(1, math.ceil(len(self.content.split()) / 200))
 
     def prepare_post(self):
+        """
+        Prepares the post by calculating reading time and setting meta fields.
+        """
         self.calculate_reading_time()
+
         if not self.meta_title:
             self.meta_title = self.title
+
         if not self.meta_description:
             self.meta_description = (self.excerpt or self.content)[:155]
+
         if not self.canonical_url:
             self.canonical_url = f"/blog/{self.slug}"
+
 
 
 # -------------------------
